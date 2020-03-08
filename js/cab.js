ccb9e362f8439f6026e0edad3306c7c5cabee76c
Daniil Trofimov <trofimovdanil946@gmail.com>
2020-03-09 01:44:19 +0300

    pesronal-cab layout

---
+++ js/cab.js
@@ -0,0 +1,6 @@
+function makeStraight() {
+    let objects = document.getElementsByClassName("indicator");
+    // функция, основываясь на длине названия показателя и его значения выравнивает поля
+}
+
+makeStraight();
