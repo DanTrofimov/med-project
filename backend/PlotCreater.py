164983944230e175b963e8d32f8eb5fd3ac27776
Azat Gilyazov <azatpscl@gmail.com>
2020-03-10 20:33:57 +0300

    Update PlotCreater.py
    -added opportunity to renaming png file
    -fixed bugs
    -added small documentation

--- backend/PlotCreater.py
+++ backend/PlotCreater.py
@@ -1,10 +1,27 @@
 import matplotlib.pyplot as plt
 
+"""
 
-def getPlot(values1, values2, title='Средний пульс за прощедшую неделю', xl='Дни Недели', yl='Средний Пульс'):
+@created by AnteeOne
+@version 1.0
+
+Module with method , which returns new plot as png file
+
+Parameters:
+    values1  - list of values , located on the axis of abscissa
+    values2  - list of values , located on the axis of ordinate (f(values1))
+    title    - title of plot
+    x1       - title of axis of abscissa
+    y1       - title of axis of ordinate
+    imgtitle - title of image png file
+    
+    
+"""
+
+def getPlot(values1, values2, title='Plot', xl='x', yl='y', imgtitle="plot"):
     fig, ax = plt.subplots()
-    ax.plot(t, s)
-    plt.tick_params(labelsize = 9)
-    ax.set(xlabel = xl,ylabel = yl,title = title)
+    ax.plot(values1, values2)
+    plt.tick_params(labelsize=9)
+    ax.set(xlabel=xl, ylabel=yl, title=title)
     ax.grid()
-    fig.savefig("resources/resources.png")
+    fig.savefig("resources/" + imgtitle)
