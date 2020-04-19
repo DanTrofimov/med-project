b3dc4b6830877bca56770b0d570fb50083359f06
Azat Gilyazov <azatpscl@gmail.com>
2020-04-19 21:07:37 +0300

    Added analyse page(url) + fixes bugs
    Added analyse page(url) + fixes bugs

--- home/views.py
+++ home/views.py
@@ -1,18 +1,21 @@
 from django.http import HttpResponse
-from django.shortcuts import render , redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate, login, logout
+
 
 @login_required(login_url='login')
 def home(request):
-    return render(request,"index.html")
+    return render(request, "index.html")
+
 
 @login_required(login_url='login')
 def news(request):
-    return render(request,"news.html")
+    return render(request, "news.html")
+
 
 def registerPage(request):
     if request.user.is_authenticated:
@@ -24,14 +27,14 @@ def registerPage(request):
             if form.is_valid():
                 form.save()
                 user = form.cleaned_data.get('username')
-                messages.success(request,'Account was created for' + user)
+                messages.success(request, 'Account was created for' + user)
                 return redirect('login')
 
-        context = {'form':form}
-        return render(request,"register.html",context)
+        context = {'form': form}
+        return render(request, "register.html", context)
+
 
 def loginPage(request):
-
     if request.user.is_authenticated:
         return redirect('home')
     else:
@@ -39,26 +42,32 @@ def loginPage(request):
             username = request.POST.get('username')
             password = request.POST.get('password')
 
-            user = authenticate(request,username=username,password=password)
+            user = authenticate(request, username=username, password=password)
 
             if user is not None:
-                login(request,user)
+                login(request, user)
                 return redirect('home')
             else:
-                messages.info(request,'Username OR password is incorrect')
+                messages.info(request, 'Username OR password is incorrect')
         context = {}
-        return render(request,"login.html",context)
+        return render(request, "login.html", context)
+
 
 def logoutUser(request):
     logout(request)
     return redirect('login')
 
+
 @login_required(login_url='login')
 def personalcab(request):
-    return render(request,'personal-cab-3.html')
+    return render(request, 'personal-cab-3.html')
+
 
 @login_required(login_url='login')
 def personalcab_changedata(request):
-    return render(request,'personal-cab-3-changedata.html')
+    return render(request, 'personal-cab-3-changedata.html')
 
 
+@login_required(login_url='login')
+def analyse(request):
+    return render(request, 'analyse.html')
